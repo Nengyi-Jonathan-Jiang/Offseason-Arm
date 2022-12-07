57b7918435d47ceba0c524dcac5bc8c8e6b3d7ab
IsGarbage <89527356+GKevinLi@users.noreply.github.com>
2022-12-06 21:09:03 -0600

    Update ElevatorSubsystem.java

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -16,27 +16,39 @@ public class ElevatorSubsystem  extends SubsystemBase{
     private final CANCoder encoder;
 
     private final PIDController pid;
+    public WPI_VictorSPX motor2;
+    private final CANCoder encoder2;
+    private final PIDController pid2;
 
     public ElevatorSubsystem() {
-        motor = new WPI_VictorSPX(Constants.ELEVATOR_MOTOR_ID);
+        motor = new WPI_VictorSPX(Constants.ELEVATOR_MOTORRIGHT_ID);
+        motor2 = new WPI_VictorSPX(Constants.ELEVATOR_MOTORLEFT_ID);
 
 
         motor.configFactoryDefault();
+        motor2.configFactoryDefault();
+        
 
         motor.setNeutralMode(NeutralMode.Brake);
+        motor2.setNeutralMode(NeutralMode.Brake);
 
-        encoder = new CANCoder(Constants.ELEVATOR_ENCODER);
+        encoder = new CANCoder(Constants.ELEVATOR_MOTORLEFT_ENCODER);
+        encoder2 = new CANCoder(Constants.ELEVATOR_MOTORRIGHT_ENCODER);
         pid = new PIDController(0.01, 0, 0);
+        pid2 = new PIDController(0.01, 0, 0);
     }
 
     public void setPosition(double position) {
         pid.setSetpoint(position);
+        pid2.setSetpoint(-position);
     }
 
     //called continuously forever
     @Override
     public void periodic() {
         double voltage = pid.calculate(encoder.getPosition());
+        double voltage2 = pid2.calculate(encoder2.getPosition());
         motor.set(voltage);
+        motor2.set(voltage2);
     }
 }
